10d872aa818963670db54099785f8ad2297abb0e
ubaiduddin <syedubaid021@gmail.com>
2022-03-30 02:16:46 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,133 @@
+// ________________________________________CHAPTER #1___________________________________________//
+// alert("Error!Please enter a valid password.");
+
+// alert("Welcome to js Land...\nHappy Coding!");
+
+// alert("Welcome to js Land...");
+
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+
+// console.log("Hello... I can run JS through my web browser's console ");
+// ________________________________________CHAPTER #2_______________________________________________//
+
+// var username;
+
+// var myName = "Syed ubaid";
+
+// var message = "JS";
+
+// var message = "HELLO WORLD";
+
+// alert(message);
+
+// var username = "Syed Ubaid";
+
+// alert(username);
+
+// var age = "18";
+
+// alert(age);
+
+// var work = "certified mobile application development";
+
+// alert(work);
+
+// var value = "pizza \npizz \npiz \npi \np";
+
+// alert(value);
+
+// var email = "my email address is " + "syedubaid021@gmail.com";
+
+// alert(email);
+
+// var Book = "I am tryinhg to leran form the book A smarter\nway to learn JavaScript. ";
+
+// alert(Book);
+
+// document.write("Yeah I can write HTML content through Java script");
+
+// var line = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(line)
+// ________________________________________CHAPTER #3______________________________________//
+// var age = "I am 15 years old";
+
+// alert(age);
+
+// var visit = "You have visited this site 14 times";
+
+// alert(visit);
+
+// var birthYear = "My birth yaer is 2003 <br/> Data type of my decleared variable is number";
+
+// document.write(birthYear)
+
+// var VisitorsName = " <b>JohnDoe </b>";
+
+// var ProductTitle = "ordered ";
+
+// var Product = "<b>5 T-shirt(s) </b>";
+
+// var store = "on XYZ Clothing store"
+// document.write(VisitorsName + ProductTitle + Product + store)
+// ________________________________________CHAPTER #4_________________________________________//
+// var variable1 = "Hello, World! ",
+//     variable2 = "Testing... ",
+//     variable3 = 42;
+
+// alert(variable1 + variable2 + variable3)
+
+// var a = "<ul> <li> My_variable <br/>";
+// var b = " <li>$my_variable<br/>";
+// var c = " <li>_my_variable<br/>";
+// var d = " <li>My_variable_example<br/>";
+// var e = " <li>myVariableExample ";
+// document.getElementById("legal variables").innerHTML = "<b>legal variables:" + a + b + c + d + e;
+
+
+// var f = "<ul> <li> var <br/>";
+// var g = " <li>first name <br/>";
+// var h = " <li>1my_example <br/>";
+// var i = " <li>@my_variable <br/>";
+// var j = " <li>#my_variable ";
+// document.getElementById("illegal variables").innerHTML = "<b>illegal variables:" + f + g + h + i + j;
+
+// var heading = "<h1>Rules for naming JS variables<h1/>";
+// document.getElementById("heading").innerHTML = heading;
+
+
+// var x = "Variable names can only contain , numbers , $ and _ , For example : $my_1stVariable";
+// document.getElementById("variables").innerHTML = x;
+
+// var y = "Variables must begin with a letter , $ or _. For example $name, _name or name";
+// document.getElementById("variable1").innerHTML = y;
+
+// var z = "Variable names are case sensitive";
+// document.getElementById("variable2").innerHTML = z;
+
+// var Z = "Variable names should not be JS keywords";
+// document.getElementById("variable3").innerHTML = Z;
+
+// ________________________________________CHAPTER #5_____________________________________//
+// var num1 = 3 + 5;
+
+// document.write("Sum of 3 and 5 is " + num1);
+
+// var num2 = 3 - 5;
+
+// document.write("<br/> Subtraction of 3 and 5 is " + num2);
+
+// var num3 = 3 * 5;
+
+// document.write("<br/> Multiplication of 3 and 5 is " + num3);
+
+// var num4 = 3 / 5;
+
+// document.write("<br/> Division of 3 and 5 is " + num4);
+
+// var num5 = 3 % 5;
+
+// document.write("<br/> Modulus of 3 and 5 is " + num5);
+
+// var value = 5;
+// var value1 = 6 + 7;
+// var value2 =
